21b7ef2c0f70e52540a79bbf5f655a93ac5d6632
Brian Wheeler <bdwheele@gmail.com>
2022-06-28 03:26:17 +0100

    allow any version of netbsd from 1.6 to current

--- ansible/unpack_netbsd.sh
+++ ansible/unpack_netbsd.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -x
 
-NETBSD=/srv/netbsd-9.2
+
 SUN3HOME=$1
+NETBSD=$2
 
 # Copy the boot stuff
 gunzip -c $NETBSD/miniroot.fs.gz > /srv/tftp/miniroot.fs
@@ -11,13 +12,14 @@ gunzip -c $NETBSD/netbsd-DISKLESS.gz > $SUN3HOME/netbsd.sun3
 
 # use the included dev directory to populate the dev directory for
 # the client.
-tar -xvf ../resources/netbsd_9.2_dev.tar -C $SUN3HOME
+tar -xvf ../resources/netbsd_dev.tar -C $SUN3HOME
 
 # install the rest of the packages.
 pushd $SUN3HOME
 for n in base etc man comp misc games text modules rescue; do
-    tar -xf $NETBSD/$n.tgz
-    echo
+    if [ -e $NETBSD/$n.tgz ]; then
+        tar -xf $NETBSD/$n.tgz
+    fi
 done
 
 exit
